ec1970d08444eec67c7a4f3775fa1844a18697ea
Ken Ang <angkendrick@gmail.com>
2015-09-24 01:09:09 -0400

    association changes

--- app/models/particular.rb
+++ app/models/particular.rb
@@ -1,3 +1,4 @@
 class Particular < ActiveRecord::Base
   belongs_to :voucher
+  validates :name, :amount, presence: :true
 end
--- app/models/requisition_particular.rb
+++ app/models/requisition_particular.rb
@@ -1,4 +1,6 @@
 class RequisitionParticular < ActiveRecord::Base
   belongs_to :requisition
   belongs_to :measurement
+
+  validates :quantity, :description, :amount, presence: :true
 end
